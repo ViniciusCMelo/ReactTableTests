d51ca79d865be306e6e6dc25e205a300ad28111e
Vinícius Melo <viniciuscardosodemelo@gmail.com>
2022-04-28 12:20:26 -0300

    :coffin: remove unused api test

--- api/src/controllers/OrderController.js
+++ api/src/controllers/OrderController.js
@@ -4,12 +4,14 @@ export default {
 
   async index(request, response) {
     const orders = await orderRepository.index();
+
     return response.json(orders);
   },
 
   async show(request, response) {
     const {id} = request.params;
     const seller = await orderRepository.find(id);
+
     return response.json(seller);
   },
 }
